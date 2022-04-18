160eb767e04b9d8799e419085949ba4f7da6db12
Emirhan Engin <me@emirhanengin.com>
2022-04-19 00:21:15 +0300

    change component-designs for sessions screen

--- src/pages/Sessions/Sessions.tsx
+++ src/pages/Sessions/Sessions.tsx
@@ -8,7 +8,12 @@ import { IconChevronDown, IconSort } from './icons'
 
 const Sessions: React.FC = () => (
   <Page>
-    <UpcomingSession />
+    <UpcomingSession
+      title="Upcoming Session"
+      description="Sahana V, Msc in Clinical Psychology"
+      date="7:30 PM - 8:30 PM"
+      onPress={() => console.log('pressed')}
+    />
 
     <Section style={styles.heading}>
       <TouchableOpacity style={styles.head}>
@@ -22,9 +27,30 @@ const Sessions: React.FC = () => (
       </TouchableOpacity>
     </Section>
 
-    <Session active />
+    <Session
+      active
+      avatar={require('../../lib/assets/avatar-2.png')}
+      name="Sahana V"
+      job="Msc in Clinical Psychology"
+      date="31st March ‘22"
+      hours="7:30 PM - 8:30 PM"
+      onPressReschedule={() => console.log('reschedule')}
+      onPressReBook={() => console.log('rebook')}
+      onPressJoinNow={() => console.log('join now')}
+      onPressViewProfile={() => console.log('view profile')}
+    />
 
-    <Session />
+    <Session
+      avatar={require('../../lib/assets/avatar-3.png')}
+      name="Sahana V"
+      job="Msc in Clinical Psychology"
+      date="31st March ‘22"
+      hours="7:30 PM - 8:30 PM"
+      onPressReschedule={() => console.log('reschedule')}
+      onPressReBook={() => console.log('rebook')}
+      onPressJoinNow={() => console.log('join now')}
+      onPressViewProfile={() => console.log('view profile')}
+    />
   </Page>
 )
 
